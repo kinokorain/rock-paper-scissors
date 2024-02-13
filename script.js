9a07d141be0ce18d46b19a82fe7103fa0fdda988
kinokorain <boriskoznev@gmail.com>
2024-02-13 12:58:08 +0300

    the programm is working. Must add checks for wrong input

--- script.js
+++ script.js
@@ -1,13 +1,131 @@
-let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(nums);
-let currMax = -9999;
-for (let i = 0; i < nums.length; i++) {
-    console.log(nums[i]);
-    if (nums[i] > currMax) {
-        currMax = nums[i];
-
-    }
-
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
-console.log(currMax);
+function GetComputerChoice() {
+    let result = getRandomInt(3);
+    if (result === 0) return "Rock";
+    if (result === 1) return "Paper";
+    if (result === 2) return "Scissors";
+}
+
+function PlayRound(playerSelection, computerSelection) {
+    console.log("The round has begun!");
+    if ((playerSelection === "ROCK") && (computerSelection === "Rock")) {
+        return "TRR";
+    }
+    if ((playerSelection == "PAPER") && (computerSelection === "Paper")) {
+        return "TPP";
+    }
+    if ((playerSelection === "SCISSORS") && (computerSelection === "Scissors")) {
+        return "TSS";
+    }
+
+    if ((playerSelection === "ROCK") && (computerSelection === "Scissors")) {
+        return "WRS";
+    }
+    if ((playerSelection == "PAPER") && (computerSelection === "Rock")) {
+        return "WPR";
+    }
+    if ((playerSelection === "SCISSORS") && (computerSelection === "Paper")) {
+        return "WSP";
+    }
+
+    if ((playerSelection === "ROCK") && (computerSelection === "Paper")) {
+        return "LRP";
+    }
+    if ((playerSelection == "PAPER") && (computerSelection === "Scissors")) {
+        return "LPS";
+    }
+    if ((playerSelection === "SCISSORS") && (computerSelection === "Rock")) {
+        return "LSR";
+    }
+    return "Wrong value"
+}
+
+function PlayGame() {
+    let playerChoice, computerChoice, roundResult;
+    let playerScore = 0, computerScore = 0;
+
+    while (playerScore + computerScore < 5) {
+        playerChoice = prompt("Rock, Paper or Scissors?");
+        computerChoice = GetComputerChoice();
+        console.log("You chose " + playerChoice);
+        console.log("Computer chose " + computerChoice);
+        roundResult = PlayRound(playerChoice.toUpperCase(), computerChoice);
+        switch (roundResult) {
+            case "TRR":
+                {
+                    console.log("Tie! Both parties chose Rock.");
+                    break;
+                }
+            case "TPP":
+                {
+                    console.log("Tie! Both parties chose Paper");
+                    break;
+                }
+            case "TSS":
+                {
+                    console.log("Tie! Both parties chose Scissors");
+                    break;
+                }
+            case "WRS":
+                {
+                    console.log("You win! Rock beats Scissors");
+                    playerScore++;
+                    console.log("Current score: " + playerScore + " : " + computerScore);
+                    break;
+                }
+            case "WPR":
+                {
+                    console.log("You win! Paper beats Rock");
+                    playerScore++;
+                    console.log("Current score: " + playerScore + " : " + computerScore);
+                    break;
+                }
+            case "WSP":
+                {
+                    console.log("You win! Scissors beats Paper");
+                    playerScore++;
+                    console.log("Current score: " + playerScore + " : " + computerScore);
+                    break;
+                }
+            case "LRP":
+                {
+                    console.log("You lost. Paper beats Rock");
+                    computerScore++;
+                    console.log("Current score: " + playerScore + " : " + computerScore);
+                    break;
+                }
+            case "LPS":
+                {
+                    console.log("You lost. Scissors beats Paper");
+                    computerScore++;
+                    console.log("Current score: " + playerScore + " : " + computerScore);
+                    break;
+                }
+            case "LSR":
+                {
+                    console.log("You lost. Rock beats Scissors");
+                    computerScore++;
+                    console.log("Current score: " + playerScore + " : " + computerScore);
+                    break;
+                }
+            default:
+                {
+                    console.log("Error. Something went wrong while playing the roud.")
+                    break;
+                }
+        }
+    }
+    if (computerScore < playerScore) {
+        console.log("Congratulations! :^) You won!!");
+        return "W";
+    }
+    else {
+        console.log("Sadge :^( You lost the game...");
+        return "L";
+    }
+}
+
+PlayGame();
